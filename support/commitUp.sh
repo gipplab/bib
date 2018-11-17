02db4eef5704af662a6b6146b0670c0e02922f92
BibBot <bibbot@ckurs.de>
2018-11-17 14:44:27 +0100

    Add gpg key

--- support/commitUp.sh
+++ support/commitUp.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 mv gipp_bibertool.bib gipp.bib
+gpg --import bibbot.asc
 git config --global user.email "bibbot@ckurs.de"
 git config --global user.name "BibBot"
-git commit -m "Reformat bib file" gipp.bib
+git config --global user.signingkey "1617C27854592471"
+git commit -m -S "Reformat bib file" gipp.bib
 git push https://$GITHUB_TOKEN@github.com/ag-gipp/bib $TRAVIS_BRANCH
