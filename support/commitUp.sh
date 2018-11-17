2309df77dbfdb85ae240ef334e4d4acdc56cb7b7
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2018-11-17 13:51:58 +0100

    Update upload script to new token name

--- support/commitUp.sh
+++ support/commitUp.sh
@@ -3,4 +3,4 @@ mv gipp_bibertool.bib gipp.bib
 git config --global user.email "bibbot@ckurs.de"
 git config --global user.name "BibBot"
 git commit -m "Reformat bib file" gipp.bib
-git push https://$GITHUBKEY@github.com/ag-gipp/bib $TRAVIS_BRANCH
+git push https://$GITHUB_TOKEN@github.com/ag-gipp/bib $TRAVIS_BRANCH
