c48995fa142c8f5295ddfbfcaa1200e1980d34e8
Moritz Schubotz <wiki@physikerwelt.de>
2018-11-17 20:04:12 +0100

    Fix commit message

--- support/commitUp.sh
+++ support/commitUp.sh
@@ -4,6 +4,6 @@ gpg --import bibbot.asc
 git config --global user.email "bibbot@ckurs.de"
 git config --global user.name "BibBot"
 git config --global user.signingkey "1617C27854592471"
-git commit -m -S "Reformat bib file" gipp.bib
+git commit -m "Reformat bib file" gipp.bib
 git remote add http https://${GITHUB_TOKEN}@github.com/ag-gipp/bib.git
 git push http HEAD:$TRAVIS_BRANCH
