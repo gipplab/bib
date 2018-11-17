3f11677c0659b3740fcc8b0e62a97dfcc8d70d23
Moritz Schubotz <wiki@physikerwelt.de>
2018-11-17 20:29:22 +0100

    Fail build on invalid bib file

--- support/compile.sh
+++ support/compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 biber --tool --configfile=src/biber/reformat.xml gipp.bib --validate-datamodel
-diff -w gipp.bib gipp_bibertool.bib || support/commitUp.sh
+diff -w gipp.bib gipp_bibertool.bib || support/commitUp.sh && exit 1
 cd src/tex
 latexmk
 cp allRefs.pdf ../../main.pdf
