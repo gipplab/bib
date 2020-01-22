f2f43d0710355e2b783eb333771482de9ca3d195
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-01-22 13:17:47 +0100

    Use docker for reliable output

--- support/compile.sh
+++ support/compile.sh
@@ -2,8 +2,9 @@
 biber --tool --configfile=src/biber/reformat.xml gipp.bib --validate-datamodel
 biber --tool --configfile=src/biber/mendeley.xml gipp.bib
 diff -w gipp.bib gipp_bibertool.bib || support/commitUp.sh
+cp gipp.bib src/tex
 cd src/tex
-latexmk
+docker run --rm -it --user="$(id -u):$(id -g)" -v "$(pwd)":/home adnrv/texlive:full latexmk
 cp allRefs.pdf ../../main.pdf
 cd ../..
 python3 support/issue.py
