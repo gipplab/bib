09558a8572803af2a77fc27e4702ca4809099085
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-06-25 22:00:39 +0200

    Add decentralized open science publication list

--- support/pubAll.sh
+++ support/pubAll.sh
@@ -2,5 +2,5 @@
 docker run --rm -it --user="$(id -u):$(id -g)" -v "$(pwd)":/home adnrv/texlive:full biber --tool --configfile=src/biber/all/filter.xml ${1-gipp}.bib
 docker run --rm -it --user="$(id -u):$(id -g)" -v "$(pwd)":/home adnrv/texlive:full biber --output-format=biblatexml --configfile=src/biber/all/clean.xml --clrmacros --tool all.bib
 ./support/xstlprocJ/xsltprocJ src/xsl/transform.xsl references.xml > all.html
-cat src/html/pub_header all.html src/html/pub_footer > docs/pubAll.html
+cat src/html/pub_header all.html src/html/pub_footer > docs/pub${1-gipp}.html
 
